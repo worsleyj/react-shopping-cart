1bdbaea8515e75261e6fa9bca5cb11b6a9b1d845
worsleyj <josephworsley23@gmail.com>
2024-10-14 15:19:22 -0400

    finalize quantity adjustment from add to card button

--- src/components/products/Card.jsx
+++ src/components/products/Card.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Card = ({ name, cartQuantity, setCartQuantity }) => {
+  const [quantity, setQuantity] = useState(0);
   return (
     <div className="product-card">
       <h1>{name}</h1>
@@ -14,15 +16,34 @@ const Card = ({ name, cartQuantity, setCartQuantity }) => {
       <div className="product-card-footer">
         <h2>$10.00</h2>
         <div className="quantity-input">
-          <input type="number"></input>
-          <button>+</button>
-          <button>-</button>
+          <input
+            type="number"
+            value={quantity}
+            onChange={(event) => setQuantity(event.target.value)}
+          ></input>
+          <button
+            onClick={() => {
+              setQuantity(parseInt(quantity) + 1);
+            }}
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              if (quantity > 0) {
+                setQuantity(parseInt(quantity) - 1);
+              }
+            }}
+          >
+            -
+          </button>
         </div>
         <button
           onClick={() => {
-            setCartQuantity(cartQuantity + 1);
-            //reset card quant
-            console.log("TEST");
+            if (quantity > 0) {
+              setCartQuantity(cartQuantity + parseInt(quantity));
+            }
+            setQuantity(0);
           }}
         >
           add to cart
