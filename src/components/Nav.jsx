4dbd59d17a57788476e3954907bfb75203bb4357
worsleyj <josephworsley23@gmail.com>
2024-10-14 13:24:27 -0400

    add card quantity display to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "../styles/Nav.css";
 
 const Nav = () => {
+  const [cartQuantity, setCartQuantity] = useState(0);
+
   return (
     <>
       <nav>
         <Link to="/">Home</Link>
         <Link to="product/default">Product</Link>
+        <div>{cartQuantity}</div>
       </nav>
     </>
   );
