aac5de97a9f51005474c09af66acc8e8ff05bf46
worsleyj <josephworsley23@gmail.com>
2024-10-17 14:29:28 -0400

    add product page randomization

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -6,6 +6,7 @@ const Product = () => {
   const [cartQuantity, setCartQuantity] = useState(0);
   let cardArray = [];
   generateCards();
+  shuffleArray(cardArray);
   return (
     <>
       <Nav cartQuantity={cartQuantity} setCartQuantity={setCartQuantity} />
@@ -13,6 +14,14 @@ const Product = () => {
       <div className="card-grid">{cardArray}</div>
     </>
   );
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i >= 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
   function generateCards() {
     cardArray = [];
     let i = 0;
